42e863203875901ae5726951f3727764b7e84696
carbuaga-spn <carbuaga@gmail.com>
2026-07-15 12:20:47 +0200

    Actualizar main.py

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ from engine.features import extraer
 from engine.scorer import calcular_score
 from engine.ranking import seleccionar_mejores
 from radars.close_radar import mostrar
+
 from config.universe import cargar_universo
+from config.radars import cargar_radar
 
 
 def main():
 
     ranking = []
 
+    radar = cargar_radar("close")
+
     tickers = cargar_universo()
 
     print()
-    print("================================")
-    print("AEGIS MVP")
-    print("================================")
+    print("===================================")
+    print("AEGIS V1")
+    print("Radar Cierre")
+    print("===================================")
     print()
 
     for ticker in tickers:
@@ -46,9 +51,18 @@ def main():
         except Exception as e:
 
             print("ERROR", ticker)
+
             print(e)
 
-    ranking = seleccionar_mejores(ranking)
+    ranking = seleccionar_mejores(
+
+        ranking,
+
+        minimo=radar["min_score"],
+
+        limite=radar["top"]
+
+    )
 
     mostrar(ranking)
 
