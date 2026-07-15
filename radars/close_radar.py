eb50dd0a9d127ff4b0b431c152022159a8c60d02
carbuaga-spn <carbuaga@gmail.com>
2026-07-15 10:13:32 +0200

    Actualizar close_radar.py

--- radars/close_radar.py
+++ radars/close_radar.py
@@ -1,1 +1,12 @@
-    close_radar.py
+    from scorer import score
+
+def radar(lista):
+
+    sesiones = [score(s) for s in lista]
+
+    sesiones.sort(
+        key=lambda x: x.action_score,
+        reverse=True
+    )
+
+    return sesiones
