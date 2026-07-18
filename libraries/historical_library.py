49caf56acf0ba886319ee4d965bdfb07912a532f
carbuaga-spn <carbuaga@gmail.com>
2026-07-18 13:43:28 +0200

    Actualizar historical_library.py

--- libraries/historical_library.py
+++ libraries/historical_library.py
@@ -9,12 +9,22 @@ def construir(datos):
 
     volumen20 = volumen_medio(datos)
 
+    ultimas20 = datos[:20]
+
+    high20 = max(float(vela["high"]) for vela in ultimas20)
+
+    low20 = min(float(vela["low"]) for vela in ultimas20)
+
     return {
 
         "ema9": ema9,
 
         "ema20": ema20,
 
-        "volumen20": volumen20
+        "volumen20": volumen20,
+
+        "high20": high20,
+
+        "low20": low20
 
     }
