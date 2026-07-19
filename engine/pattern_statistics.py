0e82a0715131773df8172e6e25feb2994b0a448c
carbuaga-spn <carbuaga@gmail.com>
2026-07-19 10:21:00 +0200

    Crear pattern_statistics.py

---
+++ engine/pattern_statistics.py
@@ -0,0 +1,9 @@
+def calcular(coincidencias):
+
+    return {
+        "win_rate": ...,
+        "avg_return_5d": ...,
+        "avg_return_10d": ...,
+        "max_drawdown": ...,
+        "confidence": ...
+    }
