a312001c364b03cf9c39636099c684b41e660421
carbuaga-spn <carbuaga@gmail.com>
2026-07-20 12:27:27 +0200

    Actualizar historical_library.py

--- libraries/historical_library.py
+++ libraries/historical_library.py
@@ -3,15 +3,11 @@ from engine.indicators import ema, volumen_medio
 
 def construir(datos):
 
-    ema9 = ema(datos, 9)
-
-    ema20 = ema(datos, 20)
-
     return {
 
-        "ema9": ema9,
+        "ema9": ema(datos, 9),
 
-        "ema20": ema20,
+        "ema20": ema(datos, 20),
 
         "volumen20": volumen_medio(datos)
 
